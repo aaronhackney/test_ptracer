f600293d9f5a3ad8657587dbe61ccc2523642f9b
aaron.hackney <aaron.hackney@rackspace.com>
2018-05-29 12:06:43 -0500

    First commit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='Packet Tracer TEST (ASDM)',
+setup(name='test_ptracer',
       version='0.1',
       description='This is an example how the ASDM interface can allow us to programatically run packet-tracer tests',
       url='http://github.com/aaronhackney/test_ptrace',
